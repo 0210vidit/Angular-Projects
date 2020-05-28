2ff94cc9592ca3164033ee3cecf40b6bfb66f367
Vidit Shah <0210vidit@gmail.com>
2020-05-28 22:14:49 +0530

    Binding

--- Angular_Max/basics/src/app/servers/servers.component.ts
+++ Angular_Max/basics/src/app/servers/servers.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allow = true;
+  serverCreationStatus = 'No Server was Created';
+  serverName = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allow = !this.allow;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created ';
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = event;
+  }
 }
